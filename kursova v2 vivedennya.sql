fa6453b5a1ddd1f929b2f4a83a0402435c9ac344
OJler <47615366+OJler@users.noreply.github.com>
2020-06-24 14:56:36 +0300

    Add files via upload

---
+++ kursova v2 vivedennya.sql
@@ -0,0 +1,8 @@
+SELECT*FROM office;
+select*from bill;
+SELECT*FROM employee left join office on office.office_id=employee.office_id;
+SELECT*FROM employee inner join office on office.office_id=employee.office_id;
+select*from article;
+select*from positions;
+select*from customer;
+select*from ordering;

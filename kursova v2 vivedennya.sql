bdb45e952f63d825a3564ae8aa8581fd8fc9f7e3
OJler <47615366+OJler@users.noreply.github.com>
2020-06-25 05:19:21 +0300

    Delete kursova v2 vivedennya.sql

--- kursova v2 vivedennya.sql
+++
@@ -1,8 +0,0 @@
-SELECT*FROM office;
-select*from bill;
-SELECT*FROM employee left join office on office.office_id=employee.office_id;
-SELECT*FROM employee inner join office on office.office_id=employee.office_id;
-select*from article;
-select*from positions;
-select*from customer;
-select*from ordering;
